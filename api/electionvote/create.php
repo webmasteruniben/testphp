051298342fb268ef923d6eeac76ac03843fcc299
Jotham Edoreh <jotham.edoreh@uniben.edu>
2020-11-14 15:45:50 +0100

    saturday vibes

---
+++ api/electionvote/create.php
@@ -0,0 +1,66 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  
+// get database connection
+include_once '../config/database.php';
+  
+// instantiate elections object
+include_once '../objects/electionvotes.php';
+  
+$database = new Database();
+$db = $database->getConnection();
+  
+$electionvote = new Electionvote($db);
+  
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+  
+// make sure data is not empty
+if(
+    !empty($data->number) &&
+    !empty($data->category_id) &&
+    !empty($data->product_id)  
+){
+  
+    // set product property values
+    $electionvote->number = $data->number;
+    $electionvote->product_id = $data->product_id;
+    $electionvote->category_id = $data->category_id;
+    $electionvote->created = date('Y-m-d H:i:s');
+  
+    // create the product
+    if($electionvote->create()){
+  
+        // set response code - 201 created
+        http_response_code(201);
+  
+        // tell the user
+        echo json_encode(array("message" => "Election Vote was created."));
+    }
+  
+    // if unable to create the product, tell the user
+    else{
+  
+        // set response code - 503 service unavailable
+        http_response_code(503);
+  
+        // tell the user
+        echo json_encode(array("message" => "Unable to create election vote."));
+    }
+}
+  
+// tell the user data is incomplete
+else{
+  
+    // set response code - 400 bad request
+    http_response_code(400);
+  
+    // tell the user
+    echo json_encode(array("message" => "Unable to create election vote. Data is incomplete."));
+}
+?>
---
+++ api/objects/electionvotes.php
@@ -0,0 +1,91 @@
+<?php
+
+class Electionvote{
+
+    //database connection and table name
+    private $conn;
+    private $table_name = "electionvotes";
+
+    //object properties
+    public $id;
+    public $number;
+    public $product_id;
+    public $category_id;
+    public $created;
+
+    //constructor
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    // used by select drop-down list
+    public function readAll(){
+        //select all data
+        $query = "SELECT
+                    id, number, product_id, category_id, created
+                FROM
+                    " . $this->table_name . "
+                ORDER BY
+                    number";
+  
+        $stmt = $this->conn->prepare( $query );
+        $stmt->execute();
+  
+        return $stmt;
+    }
+
+    // used by select drop-down list
+    public function read(){
+    
+            //select all data
+            $query = "SELECT
+            id, number, product_id, category_id, created
+        FROM
+            " . $this->table_name . "
+        ORDER BY
+            number";
+    
+        $stmt = $this->conn->prepare( $query );
+        $stmt->execute();
+    
+        return $stmt;
+    }
+
+
+    // create electionvote
+    function create(){
+    
+        // query to insert record
+        $query = "INSERT INTO
+                    " . $this->table_name . "
+                SET
+                    number=:number, product_id=:product_id, category_id=:category_id, created=:created";
+    
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+    
+        // sanitize
+        $this->name=htmlspecialchars(strip_tags($this->number));
+        $this->description=htmlspecialchars(strip_tags($this->product_id));
+        $this->status=htmlspecialchars(strip_tags($this->category_id));
+        $this->created=htmlspecialchars(strip_tags($this->created));
+    
+        // bind values
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":product_id", $this->product_id);
+        $stmt->bindParam(":category_id", $this->category_id);
+        $stmt->bindParam(":created", $this->created);
+    
+        // execute query
+        if($stmt->execute()){
+            return true;
+        }
+    
+        return false;
+        
+    }
+
+}
+
+
+?>
