0610cf75c24bc1cb7a79b298162b8bfd6569ab69
Jotham Edoreh <jotham.edoreh@uniben.edu>
2020-11-19 11:36:52 +0100

    19112020 update9

--- api/product/read_one_by_election.php
+++ api/product/read_one_by_election.php
@@ -46,7 +46,7 @@ if($num>0){
             "name" => $name,
             "description" => html_entity_decode($description),
             "price" => $price,
-            "profile_pic" => $product->profile_pic,
+            "profile_pic" => $profile_pic,
             "category_id" => $category_id,
             "category_name" => $category_name
         );
