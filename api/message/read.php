cd1d64631cc58de718be3d3a5f8103dcb7222e21
Jotham Edoreh <jotham.edoreh@uniben.edu>
2020-12-31 20:21:24 +0100

    ddeed nmessges1

---
+++ api/message/read.php
@@ -0,0 +1,66 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+  
+// database connection will be here
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/messages.php';
+  
+// instantiate database and product object
+$database = new Database();
+$db = $database->getConnection();
+  
+// initialize object
+$message = new Message($db);
+  
+// read products will be here
+// query products
+$stmt = $message->read();
+$num = $stmt->rowCount();
+  
+// check if more than 0 record found
+if($num>0){
+  
+    // products array
+    $messages_arr=array();
+    $messages_arr["records"]=array();
+  
+    // retrieve our table contents
+    // fetch() is faster than fetchAll()
+    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        // extract row
+        // this will make $row['name'] to
+        // just $name only
+        extract($row);
+  
+        $message_item=array(
+            "id" => $id,
+            "fullname" => $fullname,
+            "message" => html_entity_decode($message),
+            "subject" => $subject,
+            "created" => $created
+        );
+  
+        array_push($messagbes_arr["records"], $message_item);
+    }
+  
+    // set response code - 200 OK
+    http_response_code(200);
+  
+    // show products data in json format
+    echo json_encode($messages_arr);
+} else{
+  
+    // set response code - 404 Not found
+    http_response_code(404);
+  
+    // tell the user no products found
+    echo json_encode(
+        array("message" => "No messages found.")
+    );
+}
+
+?>
